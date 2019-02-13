814c7a7bcd9497b820b0bd5232e2efe1ed9d0f5c
amran16 <maryamg99@gmail.com>
2019-02-13 13:07:41 -0800

    added login with heroku functionality

--- extra/server/routes/apps.js
+++ extra/server/routes/apps.js
@@ -2,47 +2,48 @@ const express = require('express');
 const router = express.Router();
 const Heroku = require('heroku-client'); 
 
+const mongoose = require('mongoose');
+const User = mongoose.model('user');
 
 //console.log(process.env.HEROKU_API_TOKEN);
 const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
 
-
+//Show list
 router.get('/', (req, res) => {
-    heroku.request({
-        method: 'GET',
-        path: '/apps',
-        headers: {
-            'Foo': 'Bar'
-        },
-        parseJSON: false
-    }).then(response => {
-        res.send(response);
+    heroku.get('/apps').then(apps => {
+        res.send(apps);
     }).catch(e => {
         res.status(500).send('Errro');
-        console.log(e);
+        //console.log(e);
     });
-
 });
 
-// DELETE requests
-router.get('apps/:id', (req, res) => { 
-    heroku
-      .request({
-        method: 'delete',
-        path: '/apps/:id',
-        headers: {
-          Foo: 'Bar'
-        },
-        parseJSON: false
-      })
-      .then(response => {
-        res.send(response);
-      })
-      .catch(e => {
-        res.status(500).send('Errro');
-        console.log(e);
-      });
+//Add a new list
+router.post('/name', (req, res) => {
+    let name = req.body.name
+    //console.log(name);
+    heroku.post('/apps', { body: { name: name } }).then(app => {
+        res.send(app);
+    }).catch(e => {
+        res.status(500).json(e.body);
+        //console.log(e);
+        // console.log('=================================');
+        // console.log('error  >>>>>', e);
+        // console.log('message >>> ', e.message);
+        // console.log('body >>> ', e.body);
+    });
+});
 
+// Delete list
+router.delete('/:id', (req, res) => { 
+    //console.log('item id:', req.params.id);
+    heroku.delete('/apps/' + req.params.id).then(app => { 
+      res.send(app);
+  }).catch(e => {
+      res.status(500).send('Errro');
+      //console.log(e);
+  });
+ 
 });
 
 module.exports = router;
